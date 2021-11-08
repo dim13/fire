d578a4938906f60e24b5dd38a41023dcc200a142
Dimitri Sokolyuk <sokolyuk@gmail.com>
2021-11-08 19:23:13 +0100

    ...

--- main.go
+++ main.go
@@ -14,33 +14,38 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type fire struct {
+type Fire struct {
 	img  *image.Paletted
 	off  Toggle
 	gray Toggle
 }
 
-func seed(img *image.Paletted, c int) {
+func (f *Fire) Draw(screen *ebiten.Image) {
+	draw.Draw(screen, screen.Bounds(), f.img, image.Point{}, draw.Src)
+}
+
+func (f *Fire) Layout(outsideWidth, outsideHeight int) (int, int) {
+	p := f.img.Bounds().Max
+	return p.X, p.Y
+}
+
+func (f *Fire) Update() error {
+	Draw(f.img)
+	switch {
+	case inpututil.IsKeyJustPressed(ebiten.KeyQ):
+		return errors.New("exit")
+	case inpututil.IsKeyJustPressed(ebiten.KeyG):
+		f.gray.Toggle()
+	case inpututil.IsKeyJustPressed(ebiten.KeySpace):
+		f.off.Toggle()
+	}
+	return nil
+}
+
+func Seed(img *image.Paletted, c int) {
 	p := img.Bounds().Max
 	for x := 0; x < p.X; x++ {
 		img.SetColorIndex(x, p.Y-1, uint8(c))
-	}
-}
-
-func newFire(x, y int) *fire {
-	rand.Seed(time.Now().UnixNano())
-	img := image.NewPaletted(image.Rect(0, 0, x, y), palette)
-	seed(img, len(palette)-1)
-	return &fire{
-		img: img,
-		off: Toggle{
-			On:  func() { seed(img, 0) },
-			Off: func() { seed(img, len(palette)-1) },
-		},
-		gray: Toggle{
-			On:  func() { img.Palette = gray(palette) },
-			Off: func() { img.Palette = palette },
-		},
 	}
 }
 
@@ -58,31 +63,26 @@ func Draw(img *image.Paletted) {
 	}
 }
 
-func (f *fire) Draw(screen *ebiten.Image) {
-	draw.Draw(screen, screen.Bounds(), f.img, image.Point{}, draw.Src)
-}
-
-func (f *fire) Layout(outsideWidth, outsideHeight int) (int, int) {
-	p := f.img.Bounds().Max
-	return p.X, p.Y
-}
-
-func (f *fire) Update() error {
-	Draw(f.img)
-	switch {
-	case inpututil.IsKeyJustPressed(ebiten.KeyQ):
-		return errors.New("exit")
-	case inpututil.IsKeyJustPressed(ebiten.KeyG):
-		f.gray.Toggle()
-	case inpututil.IsKeyJustPressed(ebiten.KeySpace):
-		f.off.Toggle()
+func New(x, y int) *Fire {
+	rand.Seed(time.Now().UnixNano())
+	img := image.NewPaletted(image.Rect(0, 0, x, y), palette)
+	Seed(img, len(palette)-1)
+	return &Fire{
+		img: img,
+		off: Toggle{
+			On:  func() { Seed(img, 0) },
+			Off: func() { Seed(img, len(palette)-1) },
+		},
+		gray: Toggle{
+			On:  func() { img.Palette = gray(palette) },
+			Off: func() { img.Palette = palette },
+		},
 	}
-	return nil
 }
 
 func main() {
 	ebiten.SetWindowTitle("Doom Fire")
-	if err := ebiten.RunGame(newFire(320, 240)); err != nil {
+	if err := ebiten.RunGame(New(320, 240)); err != nil {
 		log.Println(err)
 	}
 }
